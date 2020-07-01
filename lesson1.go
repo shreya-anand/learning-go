9dd367165414d41103ab6f67f8fd574e10fdcad7
shreya-anand <67666716+shreya-anand@users.noreply.github.com>
2020-06-30 20:30:13 -0500

    Create lesson1.go

---
+++ lesson1.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	firstNames := [5]string{"Anand", "Shreya", "Deepa", "Rishya", "Chintu"}
+	lastNames := [5]string{"Anand", "Rajagopal", "Narayanan", "Suresh", "Smith"}
+	fullNames := make([]string, 5)
+	for i := 0; i < 5; i++ {
+		rand.Seed(int64(i))
+		x := firstNames[rand.Intn(5)]
+		y := lastNames[rand.Intn(5)]
+		fullNames[i] = x + " " + y
+
+	}
+	fmt.Println(fullNames)
+}
